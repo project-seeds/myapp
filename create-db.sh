b4d05d44ad97143980f93173db147da567e2c74d
Muhammad Hewedy <mhewedy@gmail.com>
2020-12-11 14:40:11 +0300

    polish

--- create-db.sh
+++ create-db.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-vermin create vagrant/ubuntu/focal64 -t ansible etc/db/playbook.yaml
-
+vermin create ubuntu/focal64 -t ansible etc/db/playbook.yaml
 
 dbip=$(vermin ip $(vermin ps -a | awk '{print $1}' | tail -n 1))
 sed "s/DB_HOST/$dbip/g" app.conf.original > app.conf
